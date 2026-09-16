4d9536e721a5488ab1a3867aca8d763fa178cba8
Manoj <vemavarapumanu@gmail.com>
2026-09-16 22:02:02 +0530

    Add CineMax PWA support

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,36 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+
+    VitePWA({
+      registerType: "autoUpdate",
+
+      manifest: {
+        name: "CineMax",
+        short_name: "CineMax",
+        description:
+          "Discover movies, upcoming releases, actors and trailers.",
+        theme_color: "#000000",
+        background_color: "#000000",
+        display: "standalone",
+
+        icons: [
+          {
+            src: "/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
